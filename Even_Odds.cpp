b3b9b6eaa513a2f4574fcfb3f17119d65a76f8da
Avijit170 <avijitdeytonmoy@gmail.com>
2026-04-07 01:07:53 +0600

    problem's ans

---
+++ Even_Odds.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    long long n,k;
+    cin>>n>>k;
+    if(k<=(n+1)/2)
+    {
+        cout<<2*k-1;
+    }
+    else
+    {
+        cout<<2*(k-(n+1)/2);
+    }
+    return 0;
+}
